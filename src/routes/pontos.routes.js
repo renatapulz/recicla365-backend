9a421ac3076675c241635e073160500f1bf99153
Renata Pulz <renata.pulz@raizen.com>
2024-08-01 02:08:29 -0300

    Criado rotas [

--- src/routes/pontos.routes.js
+++ src/routes/pontos.routes.js
@@ -8,7 +8,7 @@ PontosRoutes.get('/all', (req, res) => PontosController.getAll(req, res));
 PontosRoutes.get('/', validaToken, (req, res) => PontosController.getByUserId(req, res));
 PontosRoutes.get('/:local_id', validaToken, (req, res) => PontosController.getDetailsPoints(req, res));
 PontosRoutes.post('/', validaToken, (req, res) => PontosController.create(req, res));
-PontosRoutes.put('/', validaToken, (req, res) => PontosController.update(req, res));
-PontosRoutes.delete('/:id', validaToken, (req, res) => PontosController.delete(req, res));
+PontosRoutes.put('/:local_id', validaToken, (req, res) => PontosController.update(req, res));
+PontosRoutes.delete('/:local_id', validaToken, (req, res) => PontosController.delete(req, res));
 
 module.exports = PontosRoutes
